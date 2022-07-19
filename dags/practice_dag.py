c69580aa94f6176e8d5329926b582b4c9ecaece8
Saulo Villaseñor <86028797+saulovillasenor@users.noreply.github.com>
2022-07-19 15:03:56 -0500

    Update practice_dag.py

--- dags/practice_dag.py
+++ dags/practice_dag.py
@@ -30,6 +30,35 @@ POSTGRES_CONN_ID = "ml_conn"
 POSTGRES_TABLE_NAME = "capstone_project.user_purchase"
 
 
+def ingest_data_from_gcs(
+    gcs_bucket: str,
+    gcs_object: str,
+    postgres_table: str,
+    gcp_conn_id: str = "google_cloud_default",
+    postgres_conn_id: str = "postgres_default",
+):
+    """Ingest data from an GCS location into a postgres table.
+    
+    Args:
+        gcs_bucket (str): Name of the bucket.
+        gcs_object (str): Name of the object.
+        postgres_table (str): Name of the postgres table.
+        gcp_conn_id (str): Name of the Google Cloud connection ID.
+        postgres_conn_id (str): Name of the postgres connection ID.
+    """
+    import tempfile
+
+    gcs_hook = GCSHook(gcp_conn_id=gcp_conn_id)
+    psql_hook = PostgresHook(postgres_conn_id)
+
+    with tempfile.NamedTemporaryFile() as tmp:
+        gcs_hook.download(
+            bucket_name=gcs_bucket, object_name=gcs_object, filename=tmp.name
+        )
+        psql_hook.bulk_load(table=postgres_table, tmp_file=tmp.name)
+
+
+
 with DAG(
     dag_id=DAG_ID,
     schedule_interval="@once",
@@ -38,8 +67,12 @@ with DAG(
 ) as dag:
     start_workflow = DummyOperator(task_id="start_workflow")
     
-    
-    validate = DummyOperator(task_id="validate")
+    verify_key_existence = GCSObjectExistenceSensor(
+        task_id="verify_key_existence",
+        google_cloud_conn_id=GCP_CONN_ID,
+        bucket=GCS_BUCKET_NAME,
+        object=GCS_KEY_NAME,
+    )    
     
     
     create_table_entity = PostgresOperator(
@@ -61,9 +94,43 @@ with DAG(
     )
     
     
-    load = DummyOperator(task_id="load")
+    clear_table = PostgresOperator(
+        task_id="clear_table",
+        postgres_conn_id=POSTGRES_CONN_ID,
+        sql=f"DELETE FROM {POSTGRES_TABLE_NAME}",
+    )
+    continue_process = DummyOperator(task_id="continue_process")
     
+    ingest_data = PythonOperator(
+        task_id="ingest_data",
+        python_callable=ingest_data_from_gcs,
+        op_kwargs={
+            "gcp_conn_id": GCP_CONN_ID,
+            "postgres_conn_id": POSTGRES_CONN_ID,
+            "gcs_bucket": GCS_BUCKET_NAME,
+            "gcs_object": GCS_KEY_NAME,
+            "postgres_table": POSTGRES_TABLE_NAME,
+        },
+        trigger_rule=TriggerRule.ONE_SUCCESS,
+    )
+    
+    validate_data = BranchSQLOperator(
+        task_id="validate_data",
+        conn_id=POSTGRES_CONN_ID,
+        sql=f"SELECT COUNT(*) AS total_rows FROM {POSTGRES_TABLE_NAME}",
+        follow_task_ids_if_false=[continue_process.task_id],
+        follow_task_ids_if_true=[clear_table.task_id],
+    )
     
     end_workflow = DummyOperator(task_id="end_workflow")
     
-    start_workflow >> validate >> create_table_entity >> load >> end_workflow
+    (
+        start_workflow
+        >> verify_key_existence
+        >> create_table_entity
+        >> validate_data
+    )
+    validate_data >> [clear_table, continue_process] >> ingest_data
+    ingest_data >> end_workflow
+
+    dag.doc_md = __doc__
