d9c0f56cbe6f266698504fd3b0222c01da4773b8
Saulo Villaseñor <86028797+saulovillasenor@users.noreply.github.com>
2022-07-19 11:50:21 -0500

    Update practice_dag.py

--- dags/practice_dag.py
+++ dags/practice_dag.py
@@ -20,6 +20,15 @@ DAG_ID = "gcs_to_postgres_ingestion_workflow"
 STABILITY_STATE = "unstable"
 CLOUD_PROVIDER = "gcp"
 
+# GCP constants
+GCP_CONN_ID = "google_cloud_default"
+GCS_BUCKET_NAME = "wizeline-project-bucket"
+GCS_KEY_NAME = "user_purchase.csv"
+
+# Postgres constants
+POSTGRES_CONN_ID = "ml_conn"
+
+
 with DAG(
     dag_id=DAG_ID,
     schedule_interval="@once",
@@ -27,9 +36,33 @@ with DAG(
     tags=[CLOUD_PROVIDER, STABILITY_STATE],
 ) as dag:
     start_workflow = DummyOperator(task_id="start_workflow")
+    
+    
     validate = DummyOperator(task_id="validate")
-    prepare = DummyOperator(task_id="prepare")
+    
+    
+    create_table_entity = PostgresOperator(
+        task_id="create_table_entity",
+        postgres_conn_id=POSTGRES_CONN_ID,
+        sql=f"""
+            CREATE SCHEMA IF NOT EXISTS capstone_project;
+            CREATE TABLE IF NOT EXISTS capstone_project.user_purchase (
+                invoice_number varchar(10),
+                stock_code varchar(20),
+                detail varchar(1000),
+                quantity int,
+                invoice_date timestamp,
+                unit_price numeric(8,3),
+                customer_id int,
+                country varchar(20)
+            )
+        """,
+    )
+    
+    
     load = DummyOperator(task_id="load")
+    
+    
     end_workflow = DummyOperator(task_id="end_workflow")
     
     start_workflow >> validate >> prepare >> load >> end_workflow
